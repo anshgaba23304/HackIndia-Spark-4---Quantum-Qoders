67b6c35715dd70c8725c20cde34979901b10ce8a
anshgaba23304 <gaba23304@gmail.com>
2024-10-07 18:14:05 +0530

    removed chakraUI and added Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import TixyTicket from './contract/TixyTicket.json';
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import './index.css'; // Import the Tailwind CSS file
 
 ReactDOM.render(
-    <ChakraProvider>
+    <React.StrictMode>
         <App />
-    </ChakraProvider>,
+    </React.StrictMode>,
     document.getElementById('root')
 );
